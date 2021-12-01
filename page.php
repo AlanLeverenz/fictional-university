5e254a7ffbd0cc852f8e0c406449436e57a5b623
Alan Leverenz <awleverenz@aol.com>
2021-11-30 23:08:04 -0500

    updated private policy content

--- page.php
+++ page.php
@@ -5,10 +5,10 @@ get_header();
   while(have_posts()) {
     the_post(); ?>
 
-<div class="page-banner">
+    <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(images/ocean.jpg)"></div>
       <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">Our History</h1>
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
         <div class="page-banner__intro">
           <p>Learn how the school of your dreams got started.</p>
         </div>
@@ -22,28 +22,19 @@ get_header();
         </p>
       </div>
 
-      <div class="page-links">
+      <!-- <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
         <ul class="min-list">
           <li class="current_page_item"><a href="#">Our History</a></li>
           <li><a href="#">Our Goals</a></li>
         </ul>
-      </div>
+      </div> -->
 
       <div class="generic-content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
+        <?php the_content() ?> 
       </div>
     </div>
 
-    <div class="page-section page-section--beige">
-      <div class="container container--narrow generic-content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
-      </div>
-    </div>
-    
   <?php }
 
   get_footer();
