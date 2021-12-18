b79ecfa9b45e8716b63a649c8d8827ece07ca6e9
Alan Leverenz <awleverenz@aol.com>
2021-12-17 23:55:50 -0500

    updated past-events file again

--- page-past-events.php
+++ page-past-events.php
@@ -7,27 +7,31 @@ get_header();
   <div class="page-banner__content container container--narrow">
     <h1 class="page-banner__title">Past Events</h1>
     <div class="page-banner__intro">
-      <p>A recap of our past events</p>
+      <p>See what already happened.</p>
     </div>
   </div>
 </div>
 
 <div class="container container--narrow page-section">
 <?php 
+
   while(have_posts()) {
     the_post(); ?>
-    <div class="post-item">
-      <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 
-      <div class="metabox">
-        <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', ') ?></p>
-      </div>
-      
-      <div class="generic-content">
-        <?php the_excerpt(); ?>
-        <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
+    <div class="event-summary">
+      <a class="event-summary__date t-center" href="#">
+        <span class="event-summary__month"><?php 
+          $eventDate = new DateTime(get_field('event_date'));
+          echo $eventDate->format('M');
+        ?></span>
+        <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
+      </a>
+      <div class="event-summary__content">
+        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+        <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
       </div>
     </div>
+
   <?php }
   echo paginate_links();
 ?>
