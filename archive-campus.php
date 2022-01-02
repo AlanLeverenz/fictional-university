dc26dc22106b26fff9aa17b3bb201b84fe53fc82
Alan Leverenz <awleverenz@aol.com>
2022-01-02 10:08:23 -0500

    updated archive-campus

--- archive-campus.php
+++ archive-campus.php
@@ -8,19 +8,17 @@ pageBanner(array(
 
 <div class="container container--narrow page-section">
 
-<div class="acf-map">
-
-<?php 
-  while(have_posts()) {
-    the_post(); 
-    $mapLocation = get_field('map_location');
+  <div class="acf-map">
+    <?php 
+      while(have_posts()) {
+        the_post(); 
+        $mapLocation = get_field('map_location');
+        ?>
+        <div class="marker" data-lat="<?php echo $mapLocation['lat'] ?>" data-lng="<?php echo $mapLocation['lng'] ?>"></div>
+      <?php }
+      echo paginate_links();
     ?>
-    <div class="marker" data-lat="<?php echo $mapLocation['lat'] ?>" data-lng="<?php echo $mapLocation['lng'] ?>"></div>
-<?php }
-echo paginate_links();
-?>
-</div>
-</div>
+  </div>
 </div>
 
 <?php 
