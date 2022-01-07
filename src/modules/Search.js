89f550717f29005c151894268f2b96a6819807e1
Alan Leverenz <awleverenz@aol.com>
2022-01-06 23:18:58 -0500

    applied getJSON to searches

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -27,22 +27,30 @@ class Search {
   typingLogic() {
     if (this.searchField.val() != this.previousValue) {
       clearTimeout(this.typingTimer);
-      if (!this.isSpinnerVisible) {
-        this.resultsDiv.html('<div class="spinner-loader"></div>');
-        this.isSpinnerVisible = true;
+
+      if (this.searchField.val()) {
+        if (!this.isSpinnerVisible) {
+          this.resultsDiv.html('<div class="spinner-loader"></div>');
+          this.isSpinnerVisible = true;
+        }
+        this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+      } else {
+        this.resultsDiv.html('');
+        this.isSpinnerVisible = false;
       }
-      this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
     }
     this.previousValue = this.searchField.val();
   }
 
   getResults() {
-    this.resultsDiv.html("Imagine real search results here...");
-    this.isSpinnerVisible = false;
+
+    $.getJSON('http://fictional-university.local/wp-json/wp/v2/posts?search=' + this.searchField.val(), function (posts) {
+      alert(posts[0].title.rendered);
+    })
   }
 
   keyPressDispatcher(e) {
-    if (e.keyCode == 83 && !this.isOverlayOpen) {
+    if (e.keyCode == 83 && !this.isOverlayOpen && !$("input, textarea").is(':focus')) {
       this.openOverlay();
     }
 
