3776c2513b800e5ee5aab4aa6db4ce8a3f154a52
Alan Leverenz <awleverenz@aol.com>
2022-01-14 13:45:21 -0500

    updated html appearance for results search page, programs and campuses

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -49,14 +49,17 @@ class Search {
 
         <div class="row">
           <div class="one-third">
-            <h2>General Information</h2>
+            <h2 class="search-overlay__section-title">General Information</h2>
             ${results.generalInfo.length ? '<ul class="link-list min-list">' : "<p>No general information matches that search.</p>"}
-              ${results.generalInfo.map(item => `<li><a href="${item.link}">${item.title.rendered}</a> ${item.type == "post" ? `by ${item.authorName}` : ""}</li>`).join("")}
+              ${results.generalInfo.map(item => `<li><a href="${item.permalink}">${item.title}</a> ${item.postType == "post" ? `by ${item.authorName}` : ""}</li>`).join("")}
             ${results.generalInfo.length ? "</ul>" : ""}
           </div>
 
           <div class="one-third">
             <h2 class="search-overlay__section-title">Programs</h2>
+            ${results.programs.length ? '<ul class="link-list min-list">' : `<p>No programs match that search. <a href="${universityData.root_url}/programs">View all programs</a></p>`}
+              ${results.programs.map(item => `<li><a href="${item.permalink}">${item.title}</a></li>`).join("")}
+            ${results.programs.length ? "</ul>" : ""} 
 
             <h2 class="search-overlay__section-title">Professors</h2>
 
@@ -64,13 +67,16 @@ class Search {
 
           <div class="one-third">
             <h2 class="search-overlay__section-title">Campuses</h2>
-
+            ${results.campuses.length ? '<ul class="link-list min-list">' : `<p>No campuses match that search. <a href="${universityData.root_url}/campuses">View all campuses</a></p>`}
+              ${results.campuses.map(item => `<li><a href="${item.permalink}">${item.title}</a></li>`).join("")}
+            ${results.campuses.length ? "</ul>" : ""} 
             <h2 class="search-overlay__section-title">Events</h2>
 
           </div>
         </div>
       `)
     });
+    this.isSpinnerVisible = false;
   }
 
   keyPressDispatcher(e) {
