e60e439c0a39469330236c36e68a4bb648eb0a16
Alan Leverenz <awleverenz@aol.com>
2022-01-21 13:59:56 -0500

    removed admin bar for subscribers

--- functions.php
+++ functions.php
@@ -118,3 +118,13 @@ function redirectSubsToFrontend() {
     exit;
   }
 }
+
+// removes nav bar for subscriber
+add_action('wp_loaded', 'noSubsAdminBar');
+
+function noSubsAdminBar() {
+  $ourCurrentUser = wp_get_current_user();
+  if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+    show_admin_bar(false);
+  }
+}
