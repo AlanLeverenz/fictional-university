3ec1bc247c9985ed5a0e907be68b7c766510585e
Alan Leverenz <awleverenz@aol.com>
2022-01-24 23:19:29 -0500

    added edit and delete buttons to Notes page

--- page-my-notes.php
+++ page-my-notes.php
@@ -1,5 +1,10 @@
 <?php
 
+  if (!is_user_logged_in()) {
+    wp_redirect(esc_url(site_url('/')));
+    exit; // avoid using server resources
+  }
+
 get_header();
 
   while(have_posts()) {
@@ -8,7 +13,26 @@ get_header();
     ?>
 
     <div class="container container--narrow page-section">
-    Custom code will go here
+      <ul class="min-list link-list" id="my-notes">
+        <?php 
+          $userNotes = new WP_Query(array (
+            'post_type' => 'note',
+            'posts_per_page' => -1,
+            'author' => get_current_user_id()
+          ));
+
+          while($userNotes->have_posts()) {
+            $userNotes->the_post(); ?>
+            <li>
+              <input class="note-title-field" value="<?php echo esc_attr(get_the_title()); ?>">
+              <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i>Edit</span>
+              <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i>Delete</span> 
+              <textarea class="note-body-field"><?php echo esc_attr(wp_strip_all_tags(get_the_content())); ?></textarea>
+          </li>
+          <?php } // esc_attr secures the attribute in the html
+        ?>
+      </ul>
+
   </div>
   
 
